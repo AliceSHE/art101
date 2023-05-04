96a1fd6036beaced667d360a75deed98a5682ee4
AliceSHE <130317962+AliceSHE@users.noreply.github.com>
2023-05-03 20:29:31 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,17 +2,20 @@
 // Author: Caitlin Smith and Alice Sheinin
 // Date: 5/5/3
 
-function userNameUCSC(){
+function userNameUCSC() {
   var userName = window.prompt("Please provide your user name");
-  document.writeln("userName =", userName);
+  console.log("userName =", userName);
   //split string to array
   var nameArray = userName.split('');
-  document.writeln("nameArray =", nameArray);
+  console.log("nameArray =", nameArray);
   //sort the array
   var nameSort = nameArray.sort();
-  document.writeln("nameSort =", nameSort);
+  console.log("nameSort =", nameSort);
   //join array back to string
   var nameSorted = nameSort.join('');
-  document.writeln("nameSorted =", nameSorted); 
+  dconsole.log("nameSorted =", nameSorted);
   return nameSorted;
 }
+
+//output
+document.writeln("Thank you, I found your name: ", userNameUCSC(), "</br");
