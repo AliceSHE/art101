031ad4da4829f94ce6110c37d3a375dbee234a75
AliceSHE <130317962+AliceSHE@users.noreply.github.com>
2023-05-17 22:53:03 -0700

    .

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,20 +2,21 @@
 // Author: Caitlin Smith and Alice Sheinin
 // Date: 5/17/23
 
-//
-$("#challenge").append("<button id='c'>Button</button>");
-$("#problem").append("<button id='p'>Button</button>");
-$("#result").append("<button id='r'>Button</button>");
 
-//
+//create buttons with text labels and add to sections. 
+$("#challenge").append("<button id='c'>Challenge</button>");
+$("#problem").append("<button id='p'>Problem</button>");
+$("#result").append("<button id='r>Result</button>");
+
+//click event to the challenge buttom and toggle 
 $("#challenge").click(function(){
     $(this).parent().toggleClass("special");
 })
-//
+//click event to the problem buttom and toggle 
 $("#problem").click(function(){
     $(this).parent().toggleClass("special");
 })
-//
+//click event to the result buttom and toggle 
 $("#result").click(function(){
     $(this).parent().toggleClass("special");
 })
