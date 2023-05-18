ebfd41388bc0525071367ffbfb536dc287996966
AliceSHE <130317962+AliceSHE@users.noreply.github.com>
2023-05-17 23:21:33 -0700

    .

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -8,15 +8,15 @@ $("#challenge").append("<button id='c'>Challenge</button>");
 $("#problem").append("<button id='p'>Problem</button>");
 $("#result").append("<button id='r>Result</button>");
 
-//click event to the challenge buttom and toggle 
+//click event to the challenge button and toggle 
 $("#challenge").click(function(){
     $(this).parent().toggleClass("special");
 })
-//click event to the problem buttom and toggle 
+//click event to the problem button and toggle 
 $("#problem").click(function(){
     $(this).parent().toggleClass("special");
 })
-//click event to the result buttom and toggle 
+//click event to the result button and toggle 
 $("#result").click(function(){
     $(this).parent().toggleClass("special");
 })
